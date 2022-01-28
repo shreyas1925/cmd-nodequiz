d48ece4c2e45623b94ba685f9dd30073f5272e4a
Shreyas Shettigar <shreyasshettigar34@gmail.com>
2022-01-28 17:37:25 +0530

    adding react questions

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ async function askName() {
 
 function winner() {
     console.clear();
-    figlet(`Congrats , ${playerName} !\n new JavaScript Master`, (err, data) => {
+    figlet(`Congrats , ${playerName} !\n you are the new JavaScript Master`, (err, data) => {
         console.log(gradient.pastel.multiline(data) + '\n');
 
         console.log(
@@ -68,9 +68,80 @@ function winner() {
     });
 }
 
+async function question1() {
+    const answers = await inquirer.prompt({
+        name: 'question_1',
+        type: 'list',
+        message: 'JSX stands for?\n',
+        choices: [
+            'Javascript Source Example',
+            'Javascript Simple Extender',
+            'JavaScript XML',
+            'Javascript Extended XML',
+        ],
+    });
+
+    return handleAnswer(answers.question_1 === 'JavaScript XML');
+}
+
+async function question2() {
+    const answers = await inquirer.prompt({
+        name: 'question_2',
+        type: 'list',
+        message: 'What type of DOM does Reactjs uses\n',
+        choices: ['Open-source DOM', 'Virtual DOM', 'Real DOM', 'React DOM'],
+    });
+    return handleAnswer(answers.question_2 === 'Virtual DOM');
+}
+
+async function question3() {
+    const answers = await inquirer.prompt({
+        name: 'question_3',
+        type: 'list',
+        message: `Reactjs is built by ?\n`,
+        choices: ['Walmart', 'Flipkart', 'Facebook', 'Google'],
+    });
+
+    return handleAnswer(answers.question_3 === 'Facebook');
+}
+
+async function question4() {
+    const answers = await inquirer.prompt({
+        name: 'question_4',
+        type: 'list',
+        message: 'Which of the following is NOT a primitive type?\n',
+        choices: [
+            'boolean',
+            'number',
+            'null',
+            'object',
+        ],
+    });
+    return handleAnswer(answers.question_4 === 'object');
+}
+
+async function question5() {
+    const answers = await inquirer.prompt({
+        name: 'question_5',
+        type: 'list',
+        message:
+            'Which is the last phase of React component’s lifecycle\n',
+        choices: ['Mounting Phase', 'Updating Phase', 'Final rendering phase', 'Unmounting Phase'],
+    });
+
+    return handleAnswer(answers.question_5 === 'Unmounting Phase');
+}
+
 
 // Run it with top-level await
 console.clear();
 await welcome();
 await askName();
+// questions start
+await question1();
+await question2();
+await question3();
+await question4();
+await question5();
+// questions end
 winner();
